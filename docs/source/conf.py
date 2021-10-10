fb2e2d8a726a2dda28c93771286e857f1a475266
DPC <1372755472@qq.com>
2021-10-10 13:57:20 +0800

    update docs conf.py && docs-build.yml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,6 @@ exclude_patterns = []
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
