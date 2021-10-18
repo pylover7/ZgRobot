259fe27f3249c259d59b4445777892c763b1cbe9
pylover <1372755472@qq.com>
2021-10-18 16:54:27 +0800

    add new examples && update .gitignore

---
+++ example/menu.py
@@ -0,0 +1,31 @@
+import zgrobot
+from zgrobot.client import Client
+
+
+def create_menu(robot: zgrobot.ZgRoBot, client: Client):
+    client.create_menu({
+        "button": [
+            {
+                "type": "miniprogram",
+                "name": "打卡",
+                "url": "https://www.dengtadaka.com/website/index.html",
+                "appid": "wxf3ca7ea27608450c",
+                "pagepath": "/pages/supervise/groupDetail?id=10025"
+            },
+            {
+                "type": "view",
+                "name": "主页",
+                "url": "https://mp.weixin.qq.com/s?__biz=Mzg3NzU1MjgxOA==&mid=100002639&idx=1&sn"
+                       "=3d7e9959038e0a3d01fa3f76ba715200&chksm"
+                       "=4f207e927857f78482f85af737e719c3eef35afcba6e58510bc65a6296284c2999fda51babf0#rd "
+            },
+            {
+                "type": "click",
+                "name": "帮助",
+                "key": "help"
+            }]
+    })
+
+    @robot.key_click("help")
+    def music():
+        return "现在还没有什么帮助哦~"
