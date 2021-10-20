5702a8b7c1e9af31942c2d21012555c6cf892fb2
pylover <1372755472@qq.com>
2021-10-21 02:39:15 +0800

    test10

--- tests/test_client.py
+++ tests/test_client.py
@@ -801,10 +801,10 @@ class TestClientResourceClass(BaseTestClass):
             r = self.client.upload_news_picture(f)
         assert r == {"errcode": 0, "errmsg": "ok"}
         with open(GOD_MP4, "rb") as f:
-            r = self.client.upload_news_picture('image', f)
+            r = self.client.upload_news_picture(f)
         assert r == {"errcode": 0, "errmsg": "ok"}
         with open(GOD_PIC, 'rb') as f:
-            r = self.client.upload_news_picture('imagew', f)
+            r = self.client.upload_news_picture(f)
         assert r == {"errcode": 0, "errmsg": "ok"}
 
     @responses.activate
