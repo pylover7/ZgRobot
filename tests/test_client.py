9b8e852d80259d0cecba07c52ffddd498a017225
pylover <1372755472@qq.com>
2021-10-21 02:34:03 +0800

    test8

--- tests/test_client.py
+++ tests/test_client.py
@@ -14,9 +14,15 @@ from zgrobot.client import Client, check_error, ClientException
 from zgrobot.utils import cached_property
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-GOD_PIC = os.path.join(os.path.dirname(__file__), os.path.join("media", "123.png"))
-GOD2_PIC = os.path.join(os.path.dirname(__file__), os.path.join("media", "234.pngw"))
-GOD_MP4 = os.path.join(os.path.dirname(__file__), os.path.join("media", "456.mp4"))
+GOD_PIC = os.path.join(
+    os.path.dirname(__file__), os.path.join("media", "123.png")
+)
+GOD2_PIC = os.path.join(
+    os.path.dirname(__file__), os.path.join("media", "234.pngw")
+)
+GOD_MP4 = os.path.join(
+    os.path.dirname(__file__), os.path.join("media", "456.mp4")
+)
 TOKEN_URL = "https://api.weixin.qq.com/cgi-bin/token"
 JSON_HEADER = {'content-type': 'application/json'}
 
@@ -795,11 +801,11 @@ class TestClientResourceClass(BaseTestClass):
             r = self.client.upload_news_picture(f)
         assert r == {"errcode": 0, "errmsg": "ok"}
         with open(GOD_MP4, "rb") as f:
-            r = self.client.upload_permanent_media('image', f)
-        assert not r
+            r = self.client.upload_news_picture('image', f)
+        assert r == {"errcode": 0, "errmsg": "ok"}
         with open(GOD_PIC, 'rb') as f:
-            r = self.client.upload_permanent_media('imagew', f)
-        assert not r
+            r = self.client.upload_news_picture('imagew', f)
+        assert r == {"errcode": 0, "errmsg": "ok"}
 
     @responses.activate
     @add_token_response
@@ -880,7 +886,7 @@ class TestUploadVideoClass(BaseTestClass):
         assert isinstance(r, requests.Response)
         with open(GOD2_PIC, "rb") as f:
             r = self.client.upload_permanent_video('test', "test", f)
-        assert not r
+        assert isinstance(r, requests.Response)
 
 
 class TestMediaClass(BaseTestClass):
