4fc3980c8eb8b4248507685d2a33f44352b7f455
大叶子 <1372755472@qq.com>
2023-05-20 19:04:09 +0800

    feat(message): 修改message公共类并加以注释便于调用各个属性

--- zgrobot/messages/messages.py
+++ zgrobot/messages/messages.py
@@ -13,10 +13,10 @@ class WeChatMessage(object, metaclass=MessageMetaClass):
     用于处理微信服务器推送过来的消息
 
     Attributes:
-        message_id: 消息id，64位整型
-        target: 开发者账号（ OpenID ）
-        source: 发送方账号（ OpenID ）
-        time: 信息发送的时间，一个UNIX时间戳。
+        message_id: 【公共属性】消息id，64位整型
+        target: 【公共属性】开发者账号（ OpenID ）
+        source: 【公共属性】发送方账号（ OpenID ）
+        time: 【公共属性】信息发送的时间，一个UNIX时间戳。
     """
     message_id = IntEntry('MsgId', 0)
     target = StringEntry('ToUserName')
@@ -79,32 +79,32 @@ class Message(WeChatMessage):
     用于处理微信服务器推送过来的消息
 
     Attributes:
-        __type__: 消息类型
-        content: 文本消息内容
-        img: 图片链接
-        media_id: 图片消息媒体id，可以调用多媒体文件下载接口拉取数据
-        location_x: 地理位置维度
-        location_y: 地理位置经度
-        label: 地理位置信息
-        scale: 地图缩放大小
-        title: 消息标题
-        description: 消息描述
-        url: 消息链接
-        format: 语音格式，如amr，speex等
-        recognition: 语音识别结果，UTF8编码
-        thumb_media_id: 视频消息缩略图的媒体id，可以调用多媒体文件下载接口拉取数据
+        __type__: 【公共属性】消息类型
+        content: 【文本信息】文本消息内容
+        img: 【图片信息】图片链接
+        title: 【链接信息】消息标题
+        description: 【链接信息】消息描述
+        url: 【链接信息】消息链接
+        location_x: 【位置信息】地理位置维度
+        location_y: 【位置信息】地理位置经度
+        label: 【位置信息】地理位置信息
+        scale: 【位置信息】地图缩放大小
+        media_id: 【音频信息】消息媒体ID
+        format: 【语音信息】语音格式，如amr，speex等
+        recognition: 【语音信息】语音识别结果，UTF8编码
+        thumb_media_id: 【视频信息】视频消息缩略图的媒体id，可以调用多媒体文件下载接口拉取数据
     """
     __type__: str
     content = StringEntry('Content')
     img = StringEntry('PicUrl')
-    media_id = StringEntry('MediaId')
+    title = StringEntry('Title')
+    description = StringEntry('Description')
+    url = StringEntry('Url')
     location_x = FloatEntry('Location_X')
     location_y = FloatEntry('Location_Y')
     label = StringEntry('Label')
     scale = IntEntry('Scale')
-    title = StringEntry('Title')
-    description = StringEntry('Description')
-    url = StringEntry('Url')
+    media_id = StringEntry('MediaId')
     format = StringEntry('Format')
     recognition = StringEntry('Recognition')
     thumb_media_id = StringEntry('ThumbMediaId')
