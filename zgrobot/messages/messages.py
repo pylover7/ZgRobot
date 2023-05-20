e4c066f6f26e7ca536a024afb8cf9e9cf34fcdc5
大叶子 <1372755472@qq.com>
2023-05-20 16:11:16 +0800

    feat(message): 新增message公共类并加以注释便于调用各个属性

--- zgrobot/messages/messages.py
+++ zgrobot/messages/messages.py
@@ -9,6 +9,15 @@ class MessageMetaClass(ZgRoBotMetaClass):
 
 
 class WeChatMessage(object, metaclass=MessageMetaClass):
+    """
+    用于处理微信服务器推送过来的消息
+
+    Attributes:
+        message_id: 消息id，64位整型
+        target: 开发者账号（ OpenID ）
+        source: 发送方账号（ OpenID ）
+        time: 信息发送的时间，一个UNIX时间戳。
+    """
     message_id = IntEntry('MsgId', 0)
     target = StringEntry('ToUserName')
     source = StringEntry('FromUserName')
@@ -63,3 +72,39 @@ class VideoMessage(WeChatMessage):
 
 class UnknownMessage(WeChatMessage):
     __type__ = 'unknown'
+
+
+class Message(WeChatMessage):
+    """
+    用于处理微信服务器推送过来的消息
+
+    Attributes:
+        __type__: 消息类型
+        content: 文本消息内容
+        img: 图片链接
+        media_id: 图片消息媒体id，可以调用多媒体文件下载接口拉取数据
+        location_x: 地理位置维度
+        location_y: 地理位置经度
+        label: 地理位置信息
+        scale: 地图缩放大小
+        title: 消息标题
+        description: 消息描述
+        url: 消息链接
+        format: 语音格式，如amr，speex等
+        recognition: 语音识别结果，UTF8编码
+        thumb_media_id: 视频消息缩略图的媒体id，可以调用多媒体文件下载接口拉取数据
+    """
+    __type__: str
+    content = StringEntry('Content')
+    img = StringEntry('PicUrl')
+    media_id = StringEntry('MediaId')
+    location_x = FloatEntry('Location_X')
+    location_y = FloatEntry('Location_Y')
+    label = StringEntry('Label')
+    scale = IntEntry('Scale')
+    title = StringEntry('Title')
+    description = StringEntry('Description')
+    url = StringEntry('Url')
+    format = StringEntry('Format')
+    recognition = StringEntry('Recognition')
+    thumb_media_id = StringEntry('ThumbMediaId')
